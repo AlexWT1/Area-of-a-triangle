964973b0096634e0581756894da7b7f7c32ab0bd
Alex <Alex@DESKTOP-PIJTJQ7>
2022-02-15 19:17:19 +0300

    Добавьте файлы проекта.

---
+++ Area-of-a-triangle/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Area_of_a_triangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a, h, s;
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            h = Convert.ToDouble(textBox2.Text);
+            s = (a * h) / 2;
+            textBox3.Text = Convert.ToString(s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+
+}
